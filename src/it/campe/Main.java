3971f00b5baf34cd101a23c2ea4fbc9b93afeaf0
Ksenx73 <hello@itcampe.com>
2021-10-14 16:36:23 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,13 @@
+package it.campe;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int six = 6;
+
+        //напишите тут ваш код
+
+
+        System.out.println(six);
+    }
+}
