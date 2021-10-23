7f1c84c0e041799e4d79c5d976323a6df0f0f184
malyyboh <malyyboh@gmail.com>
2021-10-23 20:37:22 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -5,7 +5,9 @@ public class Main {
     public static void main(String[] args) {
         int six = 6;
 
-        //напишите тут ваш код
+        six++;
+        six++;
+        six++;
 
 
         System.out.println(six);
